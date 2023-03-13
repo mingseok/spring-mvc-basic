21cef830b348069c39f9b46ae0b30820a8767749
minseok <alstjr706@naver.com>
2023-03-13 16:38:31 +0900

    feat : (스프링) 요청 매핑 헨들러 어뎁터 구조

---
+++ src/main/java/com/example/springmvcbasic/basic/request/RequestBodyJsonController.java
@@ -0,0 +1,27 @@
+package com.example.springmvcbasic.basic.request;
+
+import com.example.springmvcbasic.basic.HelloData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Slf4j
+@Controller
+public class RequestBodyJsonController {
+
+    @ResponseBody
+    @PostMapping("/request-body-json")
+    public String requestBodyJson(@RequestBody HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        return "ok";
+    }
+
+    @ResponseBody
+    @PostMapping("/request-body-json-v5")
+    public HelloData requestBodyJsonV5(@RequestBody HelloData data) {
+        log.info("username={}, age={}", data.getUsername(), data.getAge());
+        return data;
+    }
+}
--- src/main/java/com/example/springmvcbasic/basic/request/RequestBodyStringController.java
+++ src/main/java/com/example/springmvcbasic/basic/request/RequestBodyStringController.java
@@ -48,4 +48,6 @@ public class RequestBodyStringController {
         log.info("messageBody={}", messageBody);
         return "ok";
     }
+
+
 }
