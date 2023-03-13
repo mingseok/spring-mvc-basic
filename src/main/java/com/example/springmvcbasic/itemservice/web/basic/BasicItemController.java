df3248ff074c4a169edac7a2dec1a94a4de0333c
minseok <alstjr706@naver.com>
2023-03-13 23:57:41 +0900

    feat : (상품 서비스) 등록 구현

--- src/main/java/com/example/springmvcbasic/itemservice/web/basic/BasicItemController.java
+++ src/main/java/com/example/springmvcbasic/itemservice/web/basic/BasicItemController.java
@@ -5,8 +5,7 @@ import com.example.springmvcbasic.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -22,6 +21,48 @@ public class BasicItemController {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "basic/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model) {
+
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+
+        // @ModelAttribute가 자동으로 addItemV1 과정을 해주는 것이다.
+        // 1) 객체를 만든다. 2) html에서 넘어온 데이터들만 set을 호출 시켜 주는 것이다.
+
+        itemRepository.save(item);
+        model.addAttribute("item", item);
+
+        return "basic/item";
     }
 
     /**
