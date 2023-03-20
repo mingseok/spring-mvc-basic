6370cface875b0e03aa9d4884e5befde1760c8f9
minseok <alstjr706@naver.com>
2023-03-20 23:27:26 +0900

    feat : (로그인 처리) 로그인 기능 구현

---
+++ src/main/java/com/example/springmvcbasic/web/login/LoginForm.java
@@ -0,0 +1,18 @@
+package com.example.springmvcbasic.web.login;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class LoginForm {
+
+    @NotEmpty
+    private String loginId;
+
+    @NotEmpty
+    private String password;
+
+
+
+}
